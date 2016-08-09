ff988fc781c09ec0f9a03cf3e4163adbecadd8e3
Darius <darius@adroiti.com>
2016-08-09 14:25:29 +0300

    add t10.cs

---
+++ t10.cs
@@ -0,0 +1,20 @@
+public class t10
+{
+
+	public void M2()
+	{
+		dynamic context = null;
+		Request.Form["ShortProp_00"] = "1";
+		Request.Form["ShortProp_01"] = "2";
+		Request.Form["ShortProp_02"] = "3";
+		Request.Form["ShortProp_03"] = "4";
+		Request.Form["ShortProp_04"] = "5";
+		Request.Form["ShortProp_05"] = "6";
+		Request.Form["ShortProp_06"] = "7";
+		Request.Form["ShortProp_07"] = "8";
+		Request.Form["ShortProp_08"] = "9";
+		Request.Form["ShortProp_09"] = "10";
+		Request.Form["ShortProp_10"] = "11";
+		Request.Form["ShortProp_11"] = "12";
+	}
+}
